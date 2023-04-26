8d2f8cf9c017a9c290992226ed69aa7a4e763aea
thakkardhavalc <thakkardhavalc@gmail.com>
2023-04-26 10:44:28 -0600

    added franc class and money class. frank and dollar class extends money class

---
+++ tdd-by-example/src/main/java/com/unittest/tddbyexample/Franc.java
@@ -0,0 +1,11 @@
+package com.unittest.tddbyexample;
+
+public class Franc extends Money{
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+}
