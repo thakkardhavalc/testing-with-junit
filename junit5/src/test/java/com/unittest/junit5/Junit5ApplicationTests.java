6dc2357e6dd1060a03f1cfd85a857d70847c6333
thakkardhavalc <thakkardhavalc@gmail.com>
2023-04-26 18:52:23 -0600

    initial commit

---
+++ junit5/src/test/java/com/unittest/junit5/Junit5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unittest.junit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
