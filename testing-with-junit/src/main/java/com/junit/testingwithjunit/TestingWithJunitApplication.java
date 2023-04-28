e56eeebbc9bcea07a61d901184894895e7496c4b
thakkardhavalc <thakkardhavalc@gmail.com>
2023-04-28 13:45:28 -0600

    intial commit

---
+++ testing-with-junit/src/main/java/com/junit/testingwithjunit/TestingWithJunitApplication.java
@@ -0,0 +1,13 @@
+package com.junit.testingwithjunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingWithJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingWithJunitApplication.class, args);
+	}
+
+}
---
+++ testing-with-junit/src/test/java/com/junit/testingwithjunit/TestingWithJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junit.testingwithjunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingWithJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
